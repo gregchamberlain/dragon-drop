5d33263c18e18a89f13c12422bb31ef2a7e506b2
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-08-30 09:10:41 -0700

    User model validations, sessions and users controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,15 @@
 class Api::SessionsController < ApplicationController
 
   def create
+    @user = User.find_by_credentials(user_params[:email], user_params[:password])
+    if @user
+      login(@user)
+    else
+      render json: ["invalid username or password"], status: 400
+    end
+  end
+
+  def destroy
+    logout
   end
 end
