e1f89f1a4425ea2d29cf2ceeb5f75581f8a0a3c3
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-08-30 22:01:23 -0700

    Added descriptions to sites

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830155638) do
+ActiveRecord::Schema.define(version: 20160831050010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,12 +39,13 @@ ActiveRecord::Schema.define(version: 20160830155638) do
   add_index "pages", ["site_id", "path"], name: "index_pages_on_site_id_and_path", unique: true, using: :btree
 
   create_table "sites", force: :cascade do |t|
-    t.string   "name",                       null: false
-    t.string   "identifier",                 null: false
-    t.integer  "user_id",                    null: false
-    t.boolean  "template",   default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "name",                        null: false
+    t.string   "identifier",                  null: false
+    t.integer  "user_id",                     null: false
+    t.boolean  "template",    default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.text     "description"
   end
 
   add_index "sites", ["identifier"], name: "index_sites_on_identifier", unique: true, using: :btree
