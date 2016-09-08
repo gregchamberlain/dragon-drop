94c723d1f54b93b8b6897e15c77bd87f5d569ded
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-09-08 11:05:53 -0700

    Fixed views endpoint

--- app/controllers/api/views_controller.rb
+++ app/controllers/api/views_controller.rb
@@ -1,6 +1,7 @@
 class Api::ViewsController < ApplicationController
 
   def view
+    @view = View.new
     @view.site_id = params[:site_id] if params[:site_id]
     @view.page_id = params[:page_id] if params[:page_id]
     if @view.save
